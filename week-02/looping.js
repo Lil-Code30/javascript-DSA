63620ce1097ce1d87a293ba74e0f2a68da3470bb
Ismael Loko <lokoismael30@gmail.com>
2025-05-03 06:18:56 -0400

    solution not working on leetcode site

--- week-02/looping.js
+++ week-02/looping.js
@@ -1,71 +1,71 @@
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class LinkedList {
-// WRITE LL CONSTRUCTOR HERE //
-    constructor(value) {
-        const newNode = new Node(value);
-        this.head = newNode;
-        this.tail = this.head;
-        this.length = 1;
+  // WRITE LL CONSTRUCTOR HERE //
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let temp = this.head;
+    for (let i = 0; i < index; i++) {
+      temp = temp.next;
+    }
+    return temp;
+  }
+  set(index, value) {
+    if (index < 0 || index >= this.length) return undefined;
+    let initialValue = this.get(index);
+    initialValue.value = value;
+  }
+
+  middleNode() {
+    let middleIndex = 0;
+
+    if (this.length % 2 === 0) {
+      middleIndex = Math.ceil(this.length / 2);
+    } else {
+      middleIndex = Math.ceil(this.length / 2) - 1;
     }
 
-    push(value) {
-        const newNode = new Node(value);
-    
-        if (!this.head) {
-          this.head = newNode;
-          this.tail = newNode;
-        } else {
-          this.tail.next = newNode;
-          this.tail = newNode;
-        }
-        this.length++;
-        return this;
-    }
-    get(index){
-        if (index < 0 || index >= this.length) return undefined;
-        let temp = this.head;
-        for(let i = 0; i < index; i++){
-            temp = temp.next;
-        }
-        return temp;
-    }
-    set(index, value){
-        if (index < 0 || index >= this.length) return undefined;
-        let initialValue = this.get(index);
-        initialValue.value = value;
-    }
+    let temp = this.get(middleIndex);
+    let pre = this.get(middleIndex - 1);
+    this.head = temp;
+    pre.next = null;
 
+    // for(let i = middleIndex; i < this.length; i++){
 
-    middleNode() {
-        let middleIndex = 0;
-        
-        if(this.length % 2 === 0){
-            middleIndex = Math.ceil(this.length/2);
-        }else{
-            middleIndex = Math.ceil(this.length/2) - 1;
-        }
-        let temp = this.get(middleIndex);
-        let pre = this.get(middleIndex - 1);
-        this.head = temp;
-        pre.next = null;
-
-        // for(let i = middleIndex; i < this.length; i++){
-            
-        //     /*🛑🛑 remaining the implementation to display the middle nodes*/  
-        // }
-        /*
+    //     /*🛑🛑 remaining the implementation to display the middle nodes*/
+    // }
+    /*
             [1,2,3,4,5] length = 5
             middleIndex = 2
             for(let i = 2; i < 5; i++ )
         */
-        return this;
-    };
+    return this;
+  }
 }
 let myLinkedList = new LinkedList(40);
 myLinkedList.push(15);
@@ -73,12 +73,13 @@ myLinkedList.push(65);
 myLinkedList.push(36);
 myLinkedList.push(52);
 myLinkedList.push(76);
+myLinkedList.push(99);
+myLinkedList.push(100);
 
-// [40,15,65,36,52,76,100]
-// [36,52,76,100]
+// [40,15,65,36,52,76]
+// [36,52,76]
 let middleNode = myLinkedList.middleNode();
 console.log(middleNode);
-
 
 /*
 
