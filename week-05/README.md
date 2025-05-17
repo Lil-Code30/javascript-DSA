5d51633d2eeaaf73b851f6d09a4e2fa83e581fe0
Ismael Loko <lokoismael30@gmail.com>
2025-05-16 21:00:12 -0400

    week 05

---
+++ week-05/README.md
@@ -0,0 +1,23 @@
+# 📚 Semaine 5 – Notes d'apprentissage
+
+## 🗓️ Date
+
+Du [17/05/2025] au [18/05/2025]
+
+---
+
+## 🧠 Concepts appris
+
+- 19. Remove Nth Node From End of List
+
+---
+
+## 💡 Points clés
+
+- Continue **Find Kth Node From End** leetcode code from week 4 Du [10/05/2025] au [11/05/2025]
+
+## 📚 resources
+
+- Leetcode : [19. Remove Nth Node From End of List](https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/)
+
+### 📝 Exemples
---
+++ week-05/removeKthFromEnd.js
@@ -0,0 +1,73 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  // WRITE LL CONSTRUCTOR HERE //
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  removeKthFromEnd(k) {
+    let slow = this.head;
+    let fast = this.head;
+
+    // move fast pointer k step ahead
+    for (let i = 1; i <= k; i++) {
+      // If the fast pointer reaches null before moving k steps, return null as k is out of range.
+      if (fast === null) {
+        return null;
+      }
+
+      fast = fast.next;
+    }
+
+    while (fast.next !== null) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+
+    //
+    let temp = slow.next;
+    slow.next = slow.next.next;
+    temp = null;
+    this.length--;
+
+    return this;
+  }
+}
+
+let myLinkedList = new LinkedList(20);
+myLinkedList.push(30);
+myLinkedList.push(40); // slow
+// myLinkedList.push(50); // slow.next
+// myLinkedList.push(60); // slow.next.next
+// myLinkedList.push(70);
+// myLinkedList.push(80);
+
+// [20,30,40,50,60,70,80] k = 4 starting from the end
+//           ^ 50
+// output : [20,30,40,60,70,80]
+
+// hint : maintain two pointer with one with a delay of n
+
+console.log(myLinkedList.removeKthFromEnd(3));
